3384a43b0c7e40c4d5ab1a792133ad9ff98727e7
Awesome Johnny <jhonattancampo@Awesome-Johnny.local>
2021-06-27 17:12:25 -0300

    Cats Cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,7 +9,7 @@ const Card = ({name, email, id}) => {
              title="Photo of a kitty staring at you"/>
         <h1 className="f3 mb2">{name}</h1>
         <h2 className="f5 fw4 gray mt0">{email}</h2>
-        <a className="link tc ph3 pv1 db bg-animate bg-purple hover-bg-light-purple white f6 br1" href="javascript:void(0);">Send Message</a>
+        <a className="link tc ph3 pv1 db bg-animate bg-purple hover-bg-light-purple white f6 br1" href="/">Send Message</a>
     </div>
 
     );
