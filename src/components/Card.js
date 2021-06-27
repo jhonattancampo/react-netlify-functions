0b329ebc7d0dc56901c16adb1fbff908c98e33b6
Awesome Johnny <jhonattancampo@Awesome-Johnny.local>
2021-06-27 17:04:35 -0300

    Cats Cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ const Card = ({name, email, id}) => {
     return (
 
     <div className="dib br3 pa3 ma2 grow bw2 shadow-5 bg-near-white">
-        <img src={`https://robohash.org/${id}?200x200&set=set4 `} className="br-100 w-50 ba b--black-10 pa2 bw1"
+        <img alt="cats-images" src={`https://robohash.org/${id}?200x200&set=set4 `} className="br-100 w-50 ba b--black-10 pa2 bw1"
              title="Photo of a kitty staring at you"/>
         <h1 className="f3 mb2">{name}</h1>
         <h2 className="f5 fw4 gray mt0">{email}</h2>
